970af2887f75c202c78a6c72a3248c458de3eb27
IVANAGUSS12 <ivanaguss12@gmail.com>
2025-10-16 09:54:03 -0300

    Update urls.py

--- autorizaciones/urls.py
+++ autorizaciones/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 from django.contrib.auth.decorators import login_required
 
 from rest_framework.routers import DefaultRouter
-from core.views import PatientViewSet, AttachmentViewSet
+from core.views import PatientViewSet, AttachmentViewSet, health
 
 router = DefaultRouter()
 router.register(r'patients', PatientViewSet, basename='patient')
@@ -20,8 +20,11 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='public/index.html'), name='public_form'),
     path('gracias.html', TemplateView.as_view(template_name='public/gracias.html'), name='gracias'),
     path('panel/', login_required(TemplateView.as_view(template_name='panel/index.html')), name='panel_index'),
+
+    # Health check para DigitalOcean
+    path('health/', health, name='health_check'),
 ]
 
-# Sólo para desarrollo local cuando USE_S3=False y DEBUG=True
+# Solo en desarrollo local cuando USE_S3=False y DEBUG=True
 if settings.DEBUG and not settings.USE_S3:
     urlpatterns += static(settings.MEDIA_URL, document_root=getattr(settings, "MEDIA_ROOT", None))
